5f7e859bb44154c2cdafc9454eddec741a2554e9
Shawn M Moore <code@sartak.org>
2023-09-16 16:02:55 -0400

    Improve example

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,10 @@
 //! `ankidb` gives you an opinionated API to Anki's database.
 //!
 //! ```rust,no_run
-//! use ankidb::Database;
-//! use ankidb::table::*;
-//! use sea_query::*;
+//! use ankidb::{Database, query::{self, AnkiExt}};
 //!
 //! let db = Database::open(&"/path/to/collection.anki2")?;
-//! let (mut stmt, bind) = db.prepare(
-//!     Query::select()
-//!         .expr(Func::count(Expr::col(Asterisk)))
-//!         .from(Revlog::Table)
-//! )?;
+//! let (mut stmt, bind) = db.prepare(query::revlog().count_star())?;
 //! let res: i64 = stmt.query_row(&*bind.as_params(), |row| row.get(0))?;
 //! assert!(res > 100);
 //! # Ok::<(), rusqlite::Error>(())
